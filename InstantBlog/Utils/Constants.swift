281f66ec99ed9644fba27bbeb7d8a326044e8402
Daniel Akinniranye <dakinnir@gmail.com>
2022-07-17 18:31:46 -0400

    Added the appIcon

---
+++ InstantBlog/Utils/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  InstantBlog
+//
+//  Created by Daniel Akinniranye on 7/17/22.
+//
+
+import Foundation
+import UIKit
+
+struct K {
+    
+    // MARK: - Colors
+    static let appColor: UIColor? = UIColor(named: "AppColor")
+    
+}
