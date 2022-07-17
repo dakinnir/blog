db4e2b42dd2de40c30e4055950f0ef7a49a009de
Daniel Akinniranye <dakinnir@gmail.com>
2022-07-17 14:23:06 -0400

    Initial Commit

---
+++ InstantBlog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstantBlog
+//
+//  Created by Daniel Akinniranye on 7/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
